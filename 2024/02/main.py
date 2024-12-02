1a5405d20d6468a6341a7b851bc8b766a966a71b
Guillaume Launay <guillaume.launay@paylead.fr>
2024-12-02 10:58:48 +0100

    Day 2

--- 2024/02/main.py
+++ 2024/02/main.py
@@ -1,23 +1,34 @@
-from collections import Counter
-left, right = [], []
+reports = []
 with open("data.txt", "r") as file:
     for line in file:
-        parts = line.strip().split(" ")
-        l, r = int(parts[0]), int(parts[-1])
-        left.append(l)
-        right.append(r)
+        reports.append([int(item) for item in line.strip().split(" ")])
 
-left.sort()
-right.sort()
-res = 0
-for i,num in enumerate(left):
-	res+= abs(num - right[i])
-print(res)
 
-count = Counter(right)
-sim = 0
+def is_safe(levels) -> bool:
+    direction = 1 if levels[0] < levels[1] else -1
+    for x in range(1,len(levels)):
+        differ = (levels[x] - levels[x-1])
+        if differ > 3  or differ < -3 or differ == 0 or differ * direction < 0:
+            return False
+    return True
+def is_safe_tolerance(levels) -> bool:
+    if is_safe(levels):
+        return True
 
-for num in left:
-    if num in count:
-      sim += num * count[num]
-print(sim)
+    for i in range(len(levels)):
+        levels_copy = levels[:i] + levels[i + 1:]
+        if is_safe(levels_copy):
+            return True
+
+count_safe = 0
+for levels in reports:
+ if is_safe(levels):
+     count_safe += 1
+print(count_safe)
+
+count_safe = 0
+for levels in reports:
+ if is_safe_tolerance(levels):
+     count_safe += 1
+print(count_safe)
+
