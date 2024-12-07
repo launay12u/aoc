ec2a0a526e5f512153cdcc220cb0f0f0294156c0
Guillaume Launay <guillaume.launay@paylead.fr>
2024-12-07 14:41:20 +0100

    Improve Day 6

--- 2024/06/main.py
+++ 2024/06/main.py
@@ -25,22 +25,25 @@ def step(pos, dx, dy):
     return (pos[0] + dx, pos[1] + dy)
 
 
-def walk(lines):
+def walk(lines,added_obstacle=None):
     height = len(lines)
     width = len(lines[0])
     visited = set()
     start, obstacles = init(lines)
+    if added_obstacle:
+        obstacles.append(added_obstacle)
     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     dir = 0
     pos = start
     while on_map(pos[0], pos[1], height, width):
-        visited.add(pos)
+        visited.add((pos,dir))
         dx, dy = directions[dir]
 
         if (pos[0] + dx, pos[1] + dy) in obstacles:
             dir = turn(dir)
             dx, dy = directions[dir]
-
+        if ((pos[0] + dx, pos[1] + dy),dir) in visited:
+            raise Exception('Loop found')
         pos = step(pos, dx, dy)
 
     return visited
@@ -71,15 +74,21 @@ def check_loop(pos, dir, obstacle, lines,obstacles):
     return False
 
 
-def add_obstacles(lines):
-    start, obstacles = init(lines)
+def get_good_obstacles(visited):
     visited = walk(lines)
-    obstacle_count = 0
+    good_obstacle = set()
+    start,_ = init(lines)
     for new_obstacle in visited:
-        if check_loop(start, 0, new_obstacle, lines,obstacles):
-            obstacle_count += 1
+        try :
+            walk(lines, new_obstacle[0])
+        except:
+            good_obstacle.add(new_obstacle[0])
 
-    return obstacle_count
+    return good_obstacle
 
-print(len(walk(lines)))
-print(add_obstacles(lines))
+visited = walk(lines)
+count_unique_visited = len(set([x[0] for x in visited]))
+print(count_unique_visited)
+good_obstacle = get_good_obstacles(visited)
+count_good_obstacle = len(good_obstacle)
+print(count_good_obstacle)
